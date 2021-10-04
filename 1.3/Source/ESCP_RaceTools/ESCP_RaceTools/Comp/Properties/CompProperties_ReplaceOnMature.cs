94f9a6f2dc66a217d349f6d7be9c4531d9f8abbd
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2021-10-04 17:28:37 +1300

    Plant buildings stuff

---
+++ 1.3/Source/ESCP_RaceTools/ESCP_RaceTools/Comp/Properties/CompProperties_ReplaceOnMature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ESCP_RaceTools
+{
+    class CompProperties_ReplaceOnMature : CompProperties
+    {
+		public CompProperties_ReplaceOnMature()
+		{
+			this.compClass = typeof(Comp_ReplaceOnMature);
+		}
+		public ThingDef matureInto = null;
+		//Probably don't spawn something that is 'stuffed'. RimWorld will get angry, and find a default stuff thing
+	}
+}
