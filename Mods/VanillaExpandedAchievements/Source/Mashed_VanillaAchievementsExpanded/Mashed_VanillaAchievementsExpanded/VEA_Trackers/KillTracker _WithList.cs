e3db6499d11ba2620b38aa53829b83480b6c8c3b
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-01-06 14:39:04 +1300

    Update to 1.0.4

---
+++ Mods/VanillaExpandedAchievements/Source/Mashed_VanillaAchievementsExpanded/Mashed_VanillaAchievementsExpanded/VEA_Trackers/KillTracker _WithList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using AchievementsExpanded;
+using System.Collections.Generic;
+
+namespace Mashed_VanillaAchievementsExpanded
+{
+    /* effectively exactly the same, just checks from lists instead of singular defs */
+    class KillTracker_WithList : KillTracker
+    {
+        public KillTracker_WithList()
+        {
+
+        }
+
+        public KillTracker_WithList(KillTracker_WithList reference) : base(reference)
+        {
+            kindDefList = reference.kindDefList;
+            raceDefList = reference.raceDefList;
+        }
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref kindDefList, "kindDefList", LookMode.Def);
+            Scribe_Collections.Look(ref raceDefList, "RaceDefList", LookMode.Def);
+        }
+
+        public override bool Trigger(Pawn pawn, DamageInfo? dinfo)
+        {
+            //base.Trigger(pawn, dinfo);
+            if (killedThings.Contains(pawn.GetUniqueLoadID()))
+                return false;
+            else
+                killedThings.Add(pawn.GetUniqueLoadID());
+            bool instigator = instigatorFactionDefs.NullOrEmpty() || (dinfo?.Instigator?.Faction?.def != null && instigatorFactionDefs.Contains(dinfo.Value.Instigator.Faction.def));
+            bool kind = kindDef is null || kindDefList.Contains(pawn.kindDef);
+            bool race = raceDef is null || raceDefList.Contains(pawn.def);
+            bool faction = factionDefs.NullOrEmpty() || (pawn.Faction != null && factionDefs.Contains(pawn.Faction.def));
+            return kind && race && faction && instigator && (count <= 1 || ++triggeredCount >= count);
+        }
+
+        public List<PawnKindDef> kindDefList;
+        public List<ThingDef> raceDefList;
+    }
+}
