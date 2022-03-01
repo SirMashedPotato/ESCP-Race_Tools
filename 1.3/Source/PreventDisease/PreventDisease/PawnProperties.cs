225e75cb27354f17fdf7673427344023a026b76d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-03-02 07:21:15 +1300

    Update to version 1.0.11

---
+++ 1.3/Source/PreventDisease/PreventDisease/PawnProperties.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace PreventDisease
+{
+    class PawnProperties : DefModExtension
+    {
+        public bool preventDisease = true;
+
+        public static PawnProperties Get(Def def)
+        {
+            return def.GetModExtension<PawnProperties>();
+        }
+    }
+}
