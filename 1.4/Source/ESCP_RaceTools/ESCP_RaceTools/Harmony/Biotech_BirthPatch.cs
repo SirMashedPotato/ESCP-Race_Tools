47bd3f40ed111fcf845b84537677b6cdefacc876
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-01-21 14:57:04 +1300

    [Assembly] ApplyBirthOutcome_Patch, PawnGenerator_Patch

---
+++ 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/Harmony/Biotech_BirthPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ESCP_RaceTools
+{
+    /// <summary>
+    /// Adds inherited bonus genes based on the fathers race
+    /// </summary>
+    [HarmonyPatch(typeof(PregnancyUtility))]
+    [HarmonyPatch("ApplyBirthOutcome")]
+    public static class PregnancyUtility_ApplyBirthOutcome_Patch
+    {
+        [HarmonyPostfix]
+        public static void ApplyBirthOutcome_Patch(ref Thing __result, Pawn father = null)
+        {
+            if (father != null)
+            {
+                if (__result != null && __result is Pawn child && !child.Dead)
+                {
+                    if (father.def != child.def)
+                    {
+                        RaceProperties props = RaceProperties.Get(father.def);
+                        if (props != null && props.hybridInheritedGene != null)
+                        {
+                            child.genes.AddGene(props.hybridInheritedGene, false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/Harmony/PawnGeneratorPatch.cs
+++ 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/Harmony/PawnGeneratorPatch.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
 
 namespace ESCP_RaceTools
 {
-
+    /// <summary>
+    /// Just adds hediffs and abilities to pawns when they are generated.
+    /// </summary>
+    [HarmonyPatch(typeof(PawnGenerator))]
+    [HarmonyPatch("GenerateInitialHediffs")]
+    public static class PawnGenerator_GenerateInitialHediffs_Patch
+    {
+        [HarmonyPostfix]
+        public static void PawnGenerator_Patch(Pawn pawn)
+        {
+            RaceProperties props = RaceProperties.Get(pawn.def);
+            if (props != null)
+            {
+                if (!props.hediffsToAdd.NullOrEmpty())
+                {
+                    foreach (HediffDef hd in props.hediffsToAdd)
+                    {
+                        pawn.health.AddHediff(hd);
+                    }
+                }
+                if (!props.abilitiesToAdd.NullOrEmpty())
+                {
+                    foreach (AbilityDef ad in props.abilitiesToAdd)
+                    {
+                        pawn.abilities.GainAbility(ad);
+                    }
+                }
+                if (!props.oneOfRandomAbility.NullOrEmpty() && Rand.Chance(props.oneOfRandomAbilityChance))
+                {
+                    pawn.abilities.GainAbility(props.oneOfRandomAbility.RandomElement());
+                }
+            }
+        }
+    }
 }
