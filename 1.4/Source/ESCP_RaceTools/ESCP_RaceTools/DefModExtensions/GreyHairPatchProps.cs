60a98dfbe9c42beaf58f59080c201c3c039f5ab4
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-02-13 11:22:36 +1300

    Grey hair fix, Orsimer age gen fix

---
+++ 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/DefModExtensions/GreyHairPatchProps.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace ESCP_RaceTools
+{
+    public class GreyHairPatchProps : DefModExtension
+    {
+        public float minAgeForGreyHair = -1;
+
+        public static GreyHairPatchProps Get(Def def)
+        {
+            return def.GetModExtension<GreyHairPatchProps>();
+        }
+    }
+}
