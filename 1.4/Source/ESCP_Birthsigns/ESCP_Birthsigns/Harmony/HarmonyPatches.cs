af19db0dd1320f18f5f22610d910cad340abe26f
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-02-12 15:38:03 +1300

    Birth sign utility change

--- 1.4/Source/ESCP_Birthsigns/ESCP_Birthsigns/Harmony/HarmonyPatches.cs
+++ 1.4/Source/ESCP_Birthsigns/ESCP_Birthsigns/Harmony/HarmonyPatches.cs
@@ -25,26 +25,8 @@ namespace ESCP_Birthsigns
             {
                 if (pawn.RaceProps.Humanlike && (BirthsignExclusion.Get(pawn.def) == null || BirthSigns_ModSettings.AllowDisabledRaces))
                 {
-
                     BirthsignSetDef signs = BirthSigns_ModSettings.CurrentSetDef;
-                    HediffDef signDef;
-                    if (!signs.additionalSigns.NullOrEmpty() && Rand.Chance(signs.additionalSignsChance))
-                    {
-                        signDef = signs.additionalSigns.RandomElement();
-                    }
-                    else
-                    {
-                        int quadrum = (int)pawn.ageTracker.BirthQuadrum;
-                        int day = pawn.ageTracker.BirthDayOfYear;
-                        int monthIndex = day;
-                        while (monthIndex - 15 > -1)    ///-1 to account for the possibility of an index of exactly 0 for 60 days
-                        {
-                            monthIndex -= 15;
-                        }
-                        monthIndex /= 5;
-                        signDef = signs.birthsignHediffs[quadrum][monthIndex];
-                    }
-                    pawn.health.AddHediff(signDef);
+                    BirthSignUtility.GenerateBirthsign(pawn, signs);
                 }
             }
         }
