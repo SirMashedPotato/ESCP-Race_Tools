381c5b184b564140d7aee9a8f0c05a2b92aa2a36
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-03-04 15:23:32 +1300

    Deadric Cult Ideo origin
    - Icons for the rest of the Daedric princes
    - ESCP faction base no longer uses vanilla Ideo origins

--- 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/Harmony/UniqueDeityNamePatch.cs
+++ 1.4/Source/ESCP_RaceTools/ESCP_RaceTools/Harmony/UniqueDeityNamePatch.cs
@@ -10,6 +10,7 @@ namespace ESCP_RaceTools
 
         /* Forces deity names for specific Ideo orgins to be picked sequentially, instead of randomly 
          * This patch only affects Ideo orgins with fixed deities
+         * This whole thing disgusts me and should really be redone
          */
         [HarmonyPatch(typeof(IdeoFoundation_Deity))]
         [HarmonyPatch("FillDeity")]
@@ -18,15 +19,14 @@ namespace ESCP_RaceTools
             [HarmonyPrefix]
             public static bool UniqueDeityNameOverridePatch(IdeoFoundation_Deity.Deity deity, ref IdeoFoundation_Deity __instance)
             {
-                if (ESCP_RaceTools_ModSettings.IdeologyDivinesNames)
+                if (__instance.ideo.StructureMeme != null)
                 {
-                    //Override Divines names
-
-                    if (__instance.ideo.StructureMeme != null)
+                    if (ESCP_RaceTools_ModSettings.IdeologyDivinesNames)
                     {
-                        var cultureProps = IdeoCultureProperties.Get(__instance.ideo.culture);
-                        if (cultureProps != null 
-                            && ((cultureProps.overrideDivines && __instance.ideo.StructureMeme.defName == "ESCP_Structure_OriginDivinesEight") 
+                        //Override Divines names
+                        IdeoCultureProperties cultureProps = IdeoCultureProperties.Get(__instance.ideo.culture);
+                        if (cultureProps != null
+                            && ((cultureProps.overrideDivines && __instance.ideo.StructureMeme.defName == "ESCP_Structure_OriginDivinesEight")
                             || (cultureProps.overrideNineDivines && __instance.ideo.StructureMeme.defName == "ESCP_Structure_OriginDivinesNine")))
                         {
                             for (int i = 0; i < 8; i++)
@@ -59,83 +59,104 @@ namespace ESCP_RaceTools
                             }
                         }
                     }
-                }
 
-                if (ESCP_RaceTools_ModSettings.DeityNameFix)
-                {
-                    /* This should possibly be redone */
-
-                    var props = IdeoOrginProperties.Get(__instance.ideo.StructureMeme);
-
-                    if (props != null && props.IgnoreDuplicateDeityNames && __instance.ideo.StructureMeme.fixedDeityNameTypes != null)
+                    if (__instance.ideo.StructureMeme.defName == "ESCP_Structure_OriginDaedricCult")
                     {
-                        /* copied from original function, related meme stuff removed */
-                        Gender supremeGender = __instance.ideo.SupremeGender;
-                        bool supremeGenderFlag = false;
-                        if (supremeGender != Gender.None)
+                        IdeoOrginProperties props = IdeoOrginProperties.Get(__instance.ideo.StructureMeme);
+                        if (props != null)
                         {
-                            deity.gender = supremeGender;
-                            supremeGenderFlag = true;
-                        }
-                        else
-                        {
-                            deity.gender = Gen.RandomEnumValue<Gender>(true);
-                        }
-                        /* new stuff */
-                        MemeDef originMeme = __instance.ideo.StructureMeme;
-                        if (props.RandomiseDeityName)
-                        {
-                            deity.name = originMeme.fixedDeityNameTypes.RandomElement().name;
-                            deity.type = originMeme.fixedDeityNameTypes.RandomElement().type;
-                            if (props.overrideDeityIcons)
+                            int index = Rand.RangeInclusive(0, props.iconPath.Count-1);
+                            deity.iconPath = props.iconPath[index];
+                            deity.name = __instance.ideo.StructureMeme.fixedDeityNameTypes[index].name;
+                            deity.type = __instance.ideo.StructureMeme.fixedDeityNameTypes[index].type;
+                            Gender supremeGender = __instance.ideo.SupremeGender;
+                            if (supremeGender != Gender.None)
                             {
-                                deity.iconPath = props.iconPath.RandomElementWithFallback(props.iconPath.First());
+                                deity.gender = supremeGender;
                             }
                             else
                             {
-                                deity.iconPath = "UI/Deities/DeityGeneric";
+                                deity.gender = Gen.RandomEnumValue<Gender>(true);
                             }
                             return false;
-                        } 
-                        else
+                        }
+                    }
+
+                    if (ESCP_RaceTools_ModSettings.DeityNameFix)
+                    {
+                        /* This should possibly be redone */
+
+                        IdeoOrginProperties props = IdeoOrginProperties.Get(__instance.ideo.StructureMeme);
+                        if (props != null && props.IgnoreDuplicateDeityNames && __instance.ideo.StructureMeme.fixedDeityNameTypes != null)
                         {
-                            if (originMeme.fixedDeityNameTypes.Count() < originMeme.deityCount.max)
+                            /* copied from original function, related meme stuff removed */
+                            Gender supremeGender = __instance.ideo.SupremeGender;
+                            bool supremeGenderFlag = false;
+                            if (supremeGender != Gender.None)
                             {
-                                Log.Warning("Diety name count mismatch, reverting to vanilla code: " + originMeme.defName);
-                                return true;
+                                deity.gender = supremeGender;
+                                supremeGenderFlag = true;
                             }
-                            for (int i = 0; i < originMeme.fixedDeityNameTypes.Count(); i++)
+                            else
                             {
-                                if (!__instance.DeitiesListForReading.Where(x => x.name == originMeme.fixedDeityNameTypes[i].name).Any())
+                                deity.gender = Gen.RandomEnumValue<Gender>(true);
+                            }
+                            /* new stuff */
+                            MemeDef originMeme = __instance.ideo.StructureMeme;
+                            if (props.RandomiseDeityName)
+                            {
+                                deity.name = originMeme.fixedDeityNameTypes.RandomElement().name;
+                                deity.type = originMeme.fixedDeityNameTypes.RandomElement().type;
+                                if (props.overrideDeityIcons)
                                 {
-                                    deity.name = originMeme.fixedDeityNameTypes[i].name;
-                                    deity.type = originMeme.fixedDeityNameTypes[i].type;
-                                    
-
-                                    if (props.overrideDeityGenders && !supremeGenderFlag && props.deityGenders.Count >= i+1)
+                                    deity.iconPath = props.iconPath.RandomElementWithFallback(props.iconPath.First());
+                                }
+                                else
+                                {
+                                    deity.iconPath = "UI/Deities/DeityGeneric";
+                                }
+                                return false;
+                            }
+                            else
+                            {
+                                if (originMeme.fixedDeityNameTypes.Count() < originMeme.deityCount.max)
+                                {
+                                    Log.Warning("Diety name count mismatch, reverting to vanilla code: " + originMeme.defName);
+                                    return true;
+                                }
+                                for (int i = 0; i < originMeme.fixedDeityNameTypes.Count(); i++)
+                                {
+                                    if (!__instance.DeitiesListForReading.Where(x => x.name == originMeme.fixedDeityNameTypes[i].name).Any())
                                     {
-                                        deity.gender = props.deityGenders[i];
+                                        deity.name = originMeme.fixedDeityNameTypes[i].name;
+                                        deity.type = originMeme.fixedDeityNameTypes[i].type;
+
+
+                                        if (props.overrideDeityGenders && !supremeGenderFlag && props.deityGenders.Count >= i + 1)
+                                        {
+                                            deity.gender = props.deityGenders[i];
+                                        }
+
+                                        if (props.overrideDeityIcons && props.iconPath.Count >= i + 1)
+                                        {
+                                            deity.iconPath = props.iconPath[i];
+                                        }
+                                        else
+                                        {
+                                            deity.iconPath = "UI/Deities/DeityGeneric";
+                                        }
+
+                                        if (props.RandomiseDeityType)
+                                        {
+                                            deity.type = originMeme.fixedDeityNameTypes.RandomElement().type;
+                                        }
+
+                                        return false;
                                     }
-
-                                    if (props.overrideDeityIcons && props.iconPath.Count >= i + 1)
-                                    {
-                                        deity.iconPath = props.iconPath[i];
-                                    } 
-                                    else
-                                    {
-                                        deity.iconPath = "UI/Deities/DeityGeneric";
-                                    }
-
-                                    if (props.RandomiseDeityType)
-                                    {
-                                        deity.type = originMeme.fixedDeityNameTypes.RandomElement().type;
-                                    }
-
-                                    return false;
                                 }
                             }
+                            Log.Warning("Unique deity name not found, reverting to vanilla code: " + originMeme.defName);
                         }
-                        Log.Warning("Unique deity name not found, reverting to vanilla code: " + originMeme.defName);
                     }
                 }
                 return true;
