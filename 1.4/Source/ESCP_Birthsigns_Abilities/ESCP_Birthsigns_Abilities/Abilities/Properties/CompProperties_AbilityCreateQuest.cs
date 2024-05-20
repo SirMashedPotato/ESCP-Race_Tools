a6d256df56da17f84cf0b159b55b1cc2ded24a34
TheoPaysRespect <magentanx1@gmail.com>
2024-05-20 17:57:35 +0200

    removing 1.4

--- 1.4/Source/ESCP_Birthsigns_Abilities/ESCP_Birthsigns_Abilities/Abilities/Properties/CompProperties_AbilityCreateQuest.cs
+++
@@ -1,9 +0,0 @@
-﻿using RimWorld;
-
-namespace ESCP_Birthsigns_Abilities
-{
-    public class CompProperties_AbilityCreateQuest : CompProperties_AbilityEffect
-    {
-        public QuestScriptDef questScript;
-    }
-}
